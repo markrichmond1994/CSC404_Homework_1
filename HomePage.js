0903b6908f56be2393fa8f75abb59c111d137e66
sjhman44 <sjhman44@users.noreply.github.com>
2020-02-24 14:29:07 -0500

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,2 +1,5 @@
-#This will display the information on how the program works.
-#This will most likely be a static popup using the alert() function. 
+//This will display the information on how the program works.
+//This will most likely be a static popup using the alert() function. 
+
+
+displayInfo();
