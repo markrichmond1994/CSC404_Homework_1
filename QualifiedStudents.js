5028f7a6fc4dcae356a2ae5e0d6b9ad761a936da
sjhman44 <sjhman44@users.noreply.github.com>
2020-02-24 14:18:38 -0500

    Create QualifiedStudents.js

---
+++ QualifiedStudents.js
@@ -0,0 +1,15 @@
+//This displays the list of qualified students by taking input of the GPA threshold , and outputing the list of students that meet this requirment.
+
+
+"use strict";
+
+
+getThreshold() //this can just be a static variable now
+
+
+
+outputGPA()
+
+
+
+
