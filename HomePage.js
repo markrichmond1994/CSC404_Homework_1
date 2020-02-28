cb237873b43b928472b762309a84308cc273e8fa
sjhman44 <sjhman44@users.noreply.github.com>
2020-02-27 19:18:51 -0500

    Fixed function definition

--- HomePage.js
+++ HomePage.js
@@ -2,6 +2,6 @@
 //This will most likely be a static popup using the alert() function. 
 
 
-displayInfo(
-alert("This is the A-Team Internship, you need a GPA higher than a 2.5 to qualify")
-);
+exports.displayInfo = function(){
+  alert("This is the A-Team Internship, you need a GPA higher than a 2.5 to qualify")
+};
