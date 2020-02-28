812d0cfb459d8960f2583448769062bb45eeb88e
Stephen <stephenhess44@gmail.com>
2020-02-27 20:25:24 -0500

    Added array of grades to test against

--- Testing.js
+++ Testing.js
@@ -7,6 +7,8 @@ const HomePage = require("./HomePage");
 const GradeInput = require("./GradeInput");
 const QualifiedStudents = require("./QualifiedStudents");
 
+//Array of grades to facilitate testing by checking against. Letter Grade, Upper Bound, Lower Bound.
+let grades = [["A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"],[100,92,89,86,82,79,76,72,69,66,62,59][93,90,87,83,80,77,73,70,67,63,60,0];
 
 //All of these will be called multiple times with varing inputs to test that the functions return the expected output
 HomePage.displayInfo();
