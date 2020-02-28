db24a772a2024acdbd30b24ddb3ea11dd2224795
sjhman44 <sjhman44@users.noreply.github.com>
2020-02-27 19:17:59 -0500

    Fixed function definitions

--- GradeInput.js
+++ GradeInput.js
@@ -1,24 +1,24 @@
 //This will take the grades using a prompt, cacluate the GPA, and add that data to an array
 
-getGrades(
+exports.getGrades = function(){
 var csc141 = prompt("What was your grade for CSC141? (Number grade)","0");
 var csc142 = prompt("What was your grade for CSC142? (Number grade)","0");
 var csc240 = prompt("What was yiur grade for CSC240? (Number grade)","0");
 var csc241 = prompt("What was your grade for CSC241? (Number grade)","0");
-);
+};
 
 
 
 
-outputGPA(
+exports.outputGPA = function(){
 var gpa = ((csc141 + csc142 + csc240 + csc241)/100) //theres a chance the math for this is wrong, i didnt calculate anything yet
-);
+};
 
 
 
 
 
 
-addToArray(
+exports.addToArray = function(){
 //idk where to start there, unsure if im making a single array with the gpa or a 2d array with both the students name and gpa
-);
+};
