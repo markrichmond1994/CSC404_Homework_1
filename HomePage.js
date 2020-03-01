20b91302ebfe0347ce415b5350dc0455d70ab322
Stephen <stephenhess44@gmail.com>
2020-02-29 21:09:13 -0500

    Added more lines to homepage

--- HomePage.js
+++ HomePage.js
@@ -3,5 +3,9 @@
 
 
 exports.displayInfo = function(){
-  alert("This is the A-Team Internship, you need a GPA higher than a 2.5 to qualify")
+  console.log("This is the A-Team Internship, you need a GPA higher than a 2.5 to qualify.\n");
+  console.log("On the second page, enter your grades for the following classes: CSC141, CSC142, CSC240, and CSC241\n");
+  console.log("The system will compute your partial GPA to see if you qualify.\n");
+  console.log("The third page will allow you to view all qualified students.");
+
 };
